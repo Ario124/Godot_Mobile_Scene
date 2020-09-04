cca0c9797239964d4b7cec596c39dbb632d9cd61
Ario124 <47329632+Ario124@users.noreply.github.com>
2020-09-04 16:07:33 +0200

    Added Player

---
+++ src/player/KinematicBody2D.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+const SPEED = 60
+const GRAVITY = 10
+const JUMP_POWER = -250
+const FLOOR = Vector2(0, -1)
+
+var velocity = Vector2()
+
+
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = SPEED
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -SPEED
+	else:
+		velocity.x = 0
+	if Input.is_action_pressed("ui_up"):
+		velocity.y = JUMP_POWER
+		
+	velocity.y += GRAVITY
+
+
+func _ready():
+	print("HEY")
